b8cd5e3dfe17b4259467ff45644425062cb6476a
cuzknothz <cuzknothz@gmail.com>
2023-06-21 18:40:31 +0700

    😍

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,7 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Cuzknothz 🥕',
+  title: 'Cuzknothz',
   description: 'Cuzknothz - Web developer based in VietNam',
 }
 
