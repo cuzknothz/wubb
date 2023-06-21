c2b11b3bcc45fbb77926a9b8369c184529c10772
cuzknothz <cuzknothz@gmail.com>
2023-06-21 16:31:02 +0700

    🤣

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Cuzknothz 🥕',
-  description: 'Cuzknothz | Web developer',
+  description: 'Cuzknothz - Web developer based in VietNam',
 }
 
 export default function RootLayout({
